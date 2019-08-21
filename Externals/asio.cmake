8e59cf006b749f78ad7f49b0dc06d0a7a07a920d
Carsten Hooker <chooker101@gmail.com>
2019-08-20 18:14:29 -0700

    Cmake adds

--- Externals/asio.cmake
+++ Externals/asio.cmake
@@ -1,17 +1,26 @@
 add_library(asio STATIC)
 
 file(GLOB_RECURSE inc
-    asio/include/*.c 
-    asio/include/*.cpp 
-    asio/include/*.h 
-    asio/include/*.hpp
+    asio/asio/include/*.c 
+    asio/asio/include/*.cpp 
+    asio/asio/include/*.h 
+    asio/asio/include/*.hpp
 )
 
 file(GLOB_RECURSE src 
-    src/*.c 
-    src/*.cpp 
-    src/*.h 
-    src/*.hpp
+    asio/asio/src/*.c 
+    asio/asio/src/*.cpp 
+    asio/asio/src/*.h 
+    asio/asio/src/*.hpp
+)
+
+add_definitions(
+	-DASIO_STANDALONE
+	-DASIO_HAS_STD_TYPE_TRAITS
+	-DASIO_HAS_STD_ADDRESSOF
+	-DASIO_HAS_STD_ARRAY
+	-DASIO_HAS_CSTDINT
+	-DASIO_HAS_STD_SHARED_PTR
 )
 
 target_sources(asio
@@ -19,3 +28,12 @@ PRIVATE
     ${inc}
     ${src}
 )
+
+target_include_directories(asio
+PUBLIC
+	"asio/asio/include"
+PRIVATE
+	"asio/asio/src"
+)
+
+source_group(asio ${CMAKE_CURRENT_SOURCE_DIR}/asio)
---
+++ Setup.ps1
@@ -0,0 +1,12 @@
+$scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
+
+cd $scriptDir
+
+if ( -not(Test-Path -Path './Build' -PathType Container))
+{	
+	mkdir Build
+}
+
+cd Build
+
+cmake -G "Visual Studio 15 2017 Win64" ..
